d06276a1ad4af6781d04e10b581b0bb4e1bbbcdd
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-03 19:27:02 +0100

    Create getCalendarEvents function at handler.js
    Create the getCalendarEvents function with Node.js module.exports. Get the access token and set it as credentials in oAuth2Client. Return list of events or error.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -81,3 +81,51 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+//Create getCalendarEvents function
+//Use Node.js module.exports to create and export the function’s logic
+module.exports.getCalendarEvents = async (event) => {
+  //get the access token
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+
+  //Set the access token as credentials in oAuth2Client
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(response);
+      }
+    );
+  })
+    .then((results) => {
+      //return list of events
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
