4701108f5a00ef2f67b01d9ca19056ec59eb9272
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-12 11:10:45 +0100

    Create test at App.test.js
    Use describe() function to create a new group. Create test with the title "renders list of events". Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,3 +2,12 @@
 import { render } from "@testing-library/react";
 //import the component, which should be tested
 import App from "../App";
+
+//create a new group/“scope” called "<App /> component" via the describe() function
+describe("<App /> component", () => {
+  //create test described as "renders list of events"
+  test("renders list of events", () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
+});
