8446ba4245746f797639cf09c81357057c87d046
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-12 14:57:15 +0100

    Update EvenList with Event component
    Import Event component into EventList.js file. Update EvenList with Event component. Use map() for iteration. Use ternary operator and in case of falsy value not render anything.

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,6 +1,18 @@
+import Event from "./Event";
+
 //create and export EventList child component
-const EventList = () => {
-  return <ul id="event-list"></ul>;
+const EventList = ({ events }) => {
+  return (
+    <ul id="event-list">
+      {/* 
+      Use map() iterative method. 
+      Make sure that the .map() loop is only executed if events is defined.
+        - If "events" is a truthy value (e.g., not null, undefined, 0, false, etc.), then return the result of the .map() loop. 
+        - If it’s not a truthy value, render a null (i.e., render nothing).
+      */}
+      {events ? events.map((event) => <Event event={event} />) : null}
+    </ul>
+  );
 };
 
 export default EventList;
