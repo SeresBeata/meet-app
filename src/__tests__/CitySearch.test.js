294b68083ef8b9256ac893824ce98d273f45a79a
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-12 16:15:07 +0100

    Create test at CitySearch.test.js
    Create test described as "renders text input". Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,3 +2,14 @@
 import { render } from "@testing-library/react";
 //import the component, which should be tested
 import CitySearch from "../components/CitySearch";
+
+//create a new group/“scope” called "<CitySearch /> component" via the describe() function
+describe("<CitySearch /> component", () => {
+  //create test described as "renders text input"
+  test("renders text input", () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const cityTextBox = CitySearchComponent.queryByRole("textbox");
+    expect(cityTextBox).toBeInTheDocument();
+    expect(cityTextBox).toHaveClass("city");
+  });
+});
