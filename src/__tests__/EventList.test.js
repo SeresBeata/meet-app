53262033db0dcddac4cced07c2038781cb9b3259
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-12 14:11:00 +0100

    Create EventList.test.js file
    Create inside of  __tests__ folder a test file called “EventList.test.js” to run tests on the “EventList.js” file. Import render() function from package and import the component, which should be tested.

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,4 @@
+//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
+import { render } from "@testing-library/react";
+//import the component, which should be tested
+import EventList from "../components/EventList";
