7d6152d46932c77821d85a64894bccc87bbf72d7
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-13 14:44:18 +0100

    Create suggestion-list at CitySearch.js
    Import useState. Create state variable, called "showSuggestions" with initial state "false". Use onFocus event handler of the <input /> element, so if the the input field gained/lost focus change the state of "showSuggestions".  Use ternary operator to render a <ul> element with a suggestions list, if "showSuggestions" is true. Expect tests  "suggestions list is hidden by default" & "renders a list of suggestions when city textbox gains focus"  to be passed.

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 //create and export CitySearch child component
 const CitySearch = () => {
+  //create state variable, called "showSuggestions" with initial state "false".
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
   return (
     <div id="city-search">
-      <input type="text" className="city" placeholder="Search for a city" />
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        //Use onFocus event handler of the <input /> element, so if the the input field gained/lost focus change the state of "showSuggestions"
+        onFocus={() => setShowSuggestions(true)}
+      />
+
+      {/* 
+      Use ternary operator: 
+      - If "showSuggestions" is true, render a <ul> element with a suggestions list. 
+      - Otherwise, render a null (show nothing/no suggestions).
+      */}
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
     </div>
   );
 };
