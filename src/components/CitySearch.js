b88b11cb45d89e780adf9825ca2f2d5537701666
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-14 14:07:06 +0100

    Change suggestions-list depending on user-input.
    Update CitySearch.js file to change the suggestions list depending on the change of input field. Create state variables, called "query" and "suggestions". Use the "query" state as the value of the input field. Create "handleInputChanged" function and set it as the callback function of "onChange" on the <input/> element. Return items from the list of "suggestions" and use "suggestion" as a key to distinguish each item.  Expect "updates list of suggestions correctly when user types in city textbox" test to be passed.

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
 //create and export CitySearch child component
-const CitySearch = () => {
+const CitySearch = ({ allLocations }) => {
   //create state variable, called "showSuggestions" with initial state "false".
   const [showSuggestions, setShowSuggestions] = useState(false);
+  //create state variable, called "query" with initial state empty string
+  const [query, setQuery] = useState("");
+  //creata state variable, called "suggestions" with initial state empty array
+  const [suggestions, setSuggestions] = useState([]);
+
+  //Create "handleInputChanged" function, to be used as the callback function of "onChange"
+  const handleInputChanged = (event) => {
+    //obtain the current value of the input field
+    const value = event.target.value;
+    //filter the "allLocations" array based on the value of the input field
+    const filteredLocations = allLocations
+      ? allLocations.filter((location) => {
+          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        })
+      : [];
+
+    //set the "query" state to the input value
+    setQuery(value);
+    //set the "suggestions" state to the filtered locations array
+    setSuggestions(filteredLocations);
+  };
 
   return (
     <div id="city-search">
@@ -13,6 +34,10 @@ const CitySearch = () => {
         placeholder="Search for a city"
         //Use onFocus event handler of the <input /> element, so if the the input field gained/lost focus change the state of "showSuggestions"
         onFocus={() => setShowSuggestions(true)}
+        //use the query state as the value of the input field
+        value={query}
+        //set "handleInputChanged" function as the callback function of "onChange" on the <input /> element
+        onChange={handleInputChanged}
       />
 
       {/* 
@@ -20,7 +45,17 @@ const CitySearch = () => {
       - If "showSuggestions" is true, render a <ul> element with a suggestions list. 
       - Otherwise, render a null (show nothing/no suggestions).
       */}
-      {showSuggestions ? <ul className="suggestions"></ul> : null}
+      {showSuggestions ? (
+        <ul className="suggestions">
+          {/*return items from the list of "suggestions" and use "suggestion" as a key to distinguish each item.  */}
+          {suggestions.map((suggestion) => {
+            return <li key={suggestion}>{suggestion}</li>;
+          })}
+          <li key="See all cities">
+            <b>See all cities</b>
+          </li>
+        </ul>
+      ) : null}
     </div>
   );
 };
