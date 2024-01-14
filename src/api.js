cbde297dd5f3f48adbd9ffe8d18dce8a187559e3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-14 13:50:24 +0100

    Create functions at api.js file
    Create and export "extractLocations" function to extract event locations out of an array of events and remove duplicates by using map(), spread operator and Set. Create and export "getEvents" function to return the mockData representing the list of all events.

--- src/api.js
+++ src/api.js
@@ -1,2 +1,16 @@
 //import mock data
 import mockData from "./mock-data";
+
+//create "extractLocations" function to extract event locations out of an array of events and remove duplicates.
+export const extractLocations = (events) => {
+  //Use map() to create a new array with only locations.
+  const extractedLocations = events.map((event) => event.location);
+  //Remove all duplicates by creating another new array using the spread operator and spreading a Set.
+  const locations = [...new Set(extractedLocations)];
+  return locations;
+};
+
+//create "getEvents" function to return the mockData representing the list of all events.
+export const getEvents = async () => {
+  return mockData;
+};
