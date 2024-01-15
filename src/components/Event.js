3a71c12b10d6d4129bf534b3de97aae7bb2d14cb
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-15 19:42:57 +0100

    Update Event.js with ternary operator
    Import useState, create state variable, called "showDetails" and update Event.js with ternary operator to hide events details by default. Expect "by default, events details section should be hidden" test to be passed.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,13 @@
+//import useState
+import { useState } from "react";
 //import mock data
 import mockData from "../mock-data";
 
 //create and export Event child component
 const Event = ({ event }) => {
+  //create state variable, called "showDetails" with initial state "false".
+  const [showDetails, setshowDetails] = useState(false);
+
   return (
     <li>
       <div>{event.summary}</div>
@@ -11,11 +16,18 @@ const Event = ({ event }) => {
       <div>
         <button>show details</button>
       </div>
-      <div id="details">
-        <div>{event.start.dateTime}</div>
-        <div>{event.start.timeZone}</div>
-        <div>{event.description}</div>
-      </div>
+      {/* 
+      Use ternary operator: 
+      - If "showDetails" is true, render details of event. 
+      - Otherwise, render a null (show nothing/no suggestions).
+      */}
+      {showDetails ? (
+        <div id="details">
+          <div>{event.start.dateTime}</div>
+          <div>{event.start.timeZone}</div>
+          <div>{event.description}</div>
+        </div>
+      ) : null}
     </li>
   );
 };
