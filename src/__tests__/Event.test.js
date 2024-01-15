7012af2a344115513594247c732cc6b124560e86
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-15 15:01:43 +0100

    Create and update Event.test.js file
    Import render() function, userEvent handler, Event component, mockData and getEvents function into Event.test.js file.

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,10 @@
+//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
+import { render } from "@testing-library/react";
+//import the userEvent handler from @testling-library/user-event
+import userEvent from "@testing-library/user-event";
+//import the component, which should be tested
+import Event from "../components/Event";
+//import mock data
+import mockData from "./mock-data";
+//import function from api.js
+import { getEvents } from "../api";
