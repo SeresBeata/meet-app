dbdc0a1848312fb858a2990f908a248fee539a62
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-15 16:49:30 +0100

    Update Event.js for event-title
    Import mockData. Create <div> element and use "event.summary" to render title of the event. Expect "renders event title by using 'summary' key" test to be passed.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,13 @@
+//import mock data
+import mockData from "../mock-data";
+
 //create and export Event child component
-const Event = () => {
-  return <li></li>;
+const Event = ({ event }) => {
+  return (
+    <li>
+      <div>{event.summary}</div>
+    </li>
+  );
 };
 
 export default Event;
