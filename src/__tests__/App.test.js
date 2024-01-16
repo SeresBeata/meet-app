93925b301a1152736da730c6699b53d44a507a40
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-16 08:22:11 +0100

    Create test at App.test.js
    Create test  described as "render number of events component".  Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -19,4 +19,9 @@ describe("<App /> component", () => {
   test("render CitySearch", () => {
     expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
   });
+
+  //create test described as "render number of events component"
+  test("render number of events component", () => {
+    expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
+  });
 });
