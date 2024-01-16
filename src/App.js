edee498e51cf6db9908f51535688cba924f2b2c3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-16 08:35:40 +0100

    Update App.js with NumberOfEvents component
    Import and integrate NumberOfEvents component. Expect "render number of events component" test to be passed.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NumberOfEvents from "./components/NumberOfEvents";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import "./App.css";
@@ -5,6 +6,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
+      <NumberOfEvents />
       <CitySearch />
       <EventList />
     </div>
