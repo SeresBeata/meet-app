614d0f5a51dc50be1057b624339a27315230781d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-16 08:41:33 +0100

    Create and update NumberOfEvents.test.js
    Import render() function,  userEvent handler and NumberOfEvents component.

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,6 @@
+//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
+import { render } from "@testing-library/react";
+//import the userEvent handler from @testling-library/user-event
+import userEvent from "@testing-library/user-event";
+//import the component, which should be tested
+import NumberOfEvents from "../components/NumberOfEvents";
