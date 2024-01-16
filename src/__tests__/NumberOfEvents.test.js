aaf37e38b0ed9040ba7aa7239ccab3ab11b8ecf2
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-16 09:34:21 +0100

    Create tests at NumberOfEvents.test.js
    Create "number of events is 32 by default" and "value of textbox component changes when user types in it" tests. Run the tests by inputting the "npm run test" command in the terminal and expect them to fail.

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -19,4 +19,21 @@ describe("<NumberOfEvents/> component", () => {
       NumberOfEventsComponent.queryByRole("textbox");
     expect(NumberOfEventsTextBox).toBeInTheDocument();
   });
+
+  //create test described as "number of events is 32 by default"
+  test("number of events is 32 by default", () => {
+    const NumberOfEventsTextBox =
+      NumberOfEventsComponent.queryByRole("textbox");
+    expect(NumberOfEventsTextBox.value).toBe("32");
+  });
+
+  //create test described as "value of textbox component changes when user types in it"
+  test("value of textbox component changes when user types in it", async () => {
+    const NumberOfEventsTextBox =
+      NumberOfEventsComponent.queryByRole("textbox");
+
+    const user = userEvent.setup();
+    await user.type(NumberOfEventsTextBox, "{backspace}{backspace}10");
+    expect(NumberOfEventsTextBox.value).toBe("10");
+  });
 });
