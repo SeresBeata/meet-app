eb5a0fef5379456bdf65acd549bc23454482cacf
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-16 09:45:06 +0100

    Update <input> of NumberOfEvents.js
    Import  useState. Create state variable, called "eventsNumber" with initial state 32. Create handleInputChanged function and use it in case of input change (if user types in textbox). Expect "number of events is 32 by default" and "value of textbox component changes when user types in it" tests to be passed.

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 //create and export NumberOfEvents child component
 const NumberOfEvents = () => {
+  //create state variable, called "eventsNumber" with initial state 32.
+  const [eventsNumber, setEventsNumber] = useState(32);
+
+  //create function
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setEventsNumber(value);
+  };
+
   return (
     <div id="number-of-events">
-      <input type="text" />
+      <input
+        type="text"
+        value={eventsNumber}
+        onChange={handleInputChanged}
+        placeholder="32"
+      />
     </div>
   );
 };
