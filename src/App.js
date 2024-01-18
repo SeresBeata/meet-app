4cdfa8a1b09085344b3b9634f45c71d8dccb8bbb
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-18 10:48:34 +0100

    Create "fetchData" function at App.js
    Import "useEffect","useState" and "getEvents". Create state variables, called "events" and "currentNOE". Pass the events state as a prop to EventList. Create fetchData function to populate the "events" state with the events list, that will be fetched. Use useEffect to call fetchData. Expect test "renders a list of 32 events when the app is mounted and rendered" to be passed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import NumberOfEvents from "./components/NumberOfEvents";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+//import function from api.js
+import { getEvents } from "./api";
 import "./App.css";
 
 const App = () => {
+  //create state variable, called "events" with initial state [].
+  const [events, setEvents] = useState([]);
+  //create state variable, called "currentNOE" (abbreviation for current number of events) with initial state 32.
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  //call fetchData in useEffect()
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //create fetchData function to populate the "events" state with the events list, that will be fetched
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
   return (
     <div className="App">
       <NumberOfEvents />
       <CitySearch />
-      <EventList />
+      {/* pass the events state as a prop to EventList: */}
+      <EventList events={events} />
     </div>
   );
 };
