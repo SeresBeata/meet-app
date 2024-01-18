281c10988d4c5a336598308b85cc101fa195feb4
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-18 18:45:57 +0100

    Create getAccessToken() at api.js
    Create and export a new async function called getAccessToken. Try to find an access token in the localStorage.   Check for an authorization code, if no token is found (!accessToken).  Redirect user to the Google Authorization screen, where they can sign in and receive their authorization code, if no authorization code is found (!code).

--- src/api.js
+++ src/api.js
@@ -13,4 +13,28 @@ export const extractLocations = (events) => {
 //create "getEvents" function to return the mockData representing the list of all events.
 export const getEvents = async () => {
   return mockData;
+
+//create and export a new async function to get the access token
+export const getAccessToken = async () => {
+  // try to retrieve the access token
+  const accessToken = localStorage.getItem("access_token");
+  //check whether an access token was found in localStorage
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+  //checks for an authorization code, if no token is found (!accessToken)
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    //redirect user to the Google Authorization screen, where they can sign in and receive their authorization code, if no authorization code is found (!code)
+    if (!code) {
+      const response = await fetch(
+        "https://l679h29nq0.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 };
