429d480bfd9ea94a1dc0659c385764aab34b9cc1
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-18 09:58:26 +0100

    Create new scope & test at EventList.test.js
    Import App parent component for integration testing. Import "within" and "waitfor" functions. Use describe() function and create new group called "<EventList /> integration".  Create new  test described as "renders a list of 32 events when the app is mounted and rendered".  Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,10 +1,13 @@
-//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
-import { render } from "@testing-library/react";
+//import functions from the @testing-library/react package to mock an accurate representation of the original React component
+import { render, within, waitFor } from "@testing-library/react";
 //import the component, which should be tested
 import EventList from "../components/EventList";
 //import function from api.js
 import { getEvents } from "../api";
+//import App parent component for integration testing
+import App from "../App";
 
+//UNIT TESTS
 //create a new group/“scope” called "<EventList /> component" via the describe() function
 describe("<EventList /> component", () => {
   let EventListComponent;
@@ -24,5 +27,20 @@ describe("<EventList /> component", () => {
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
       allEvents.length
     );
+  });
+});
+
+//INTEGRATION TESTS
+//create a new group/“scope” called "<EventList /> integration" via the describe() function
+describe("<EventList /> integration", () => {
+  //create test described as "renders a list of 32 events when the app is mounted and rendered"
+  test("renders a list of 32 events when the app is mounted and rendered", async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
