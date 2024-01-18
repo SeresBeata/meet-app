389795c6c40efe3dca2ac86c0f88239b1e8ade54
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-18 14:49:43 +0100

    Create integration test at App.test.js
    Import render, within functions and userEvent handler and getEvents function. Use describe() function to create new group called "<App /> integration". Create test described as "renders a list of events matching the city selected by the user". Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,13 @@
-//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
-import { render } from "@testing-library/react";
+//import functions from the @testing-library/react package to mock an accurate representation of the original React component
+import { render, within } from "@testing-library/react";
+//import the userEvent handler from @testling-library/user-event
+import userEvent from "@testing-library/user-event";
+//import function from api.js
+import { getEvents } from "../api";
 //import the component, which should be tested
 import App from "../App";
 
+//UNIT TESTS
 //create a new group/“scope” called "<App /> component" via the describe() function
 describe("<App /> component", () => {
   let AppDOM;
@@ -23,5 +28,39 @@ describe("<App /> component", () => {
   //create test described as "render number of events component"
   test("render number of events component", () => {
     expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
+  });
+});
+
+//INTEGRATION TESTS
+//create a new group/“scope” called "<App /> integration" via the describe() function
+describe("<App /> integration", () => {
+  //create test described as "renders a list of events matching the city selected by the user"
+  test("renders a list of events matching the city selected by the user", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector("#city-search");
+    const CitySearchInput = within(CitySearchDOM).queryByRole("textbox");
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem =
+      within(CitySearchDOM).queryByText("Berlin, Germany");
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    const allRenderedEventItems =
+      within(EventListDOM).queryAllByRole("listitem");
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      (event) => event.location === "Berlin, Germany"
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+
+    allRenderedEventItems.forEach((event) => {
+      expect(event.textContent).toContain("Berlin, Germany");
+    });
   });
 });
