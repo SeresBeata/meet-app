8b36a5a61a3390b6e291272c565b8d945acd739b
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-19 13:42:46 +0100

    Create integration test at NumberOfEvents.test.js
    Import within, waitFor functions. Import App parent component. Use  describe() function to create new group called "<NumberOfEvents /> integration". Create test described as "renders a new number of events based on what the user types as a number". Run the test by inputting the "npm run test" command in the terminal and expect it to fail.

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,10 +1,13 @@
-//import render() function from the @testing-library/react package to mock an accurate representation of the original React component
-import { render } from "@testing-library/react";
+//import functions from the @testing-library/react package to mock an accurate representation of the original React component
+import { render, within, waitFor } from "@testing-library/react";
 //import the userEvent handler from @testling-library/user-event
 import userEvent from "@testing-library/user-event";
 //import the component, which should be tested
 import NumberOfEvents from "../components/NumberOfEvents";
+//import App parent component for integration testing
+import App from "../App";
 
+//UNIT TESTS
 //create a new group/“scope” called "<NumberOfEvents /> component" via the describe() function
 describe("<NumberOfEvents/> component", () => {
   let NumberOfEventsComponent;
@@ -35,5 +38,27 @@ describe("<NumberOfEvents/> component", () => {
     const user = userEvent.setup();
     await user.type(NumberOfEventsTextBox, "{backspace}{backspace}10");
     expect(NumberOfEventsTextBox.value).toBe("10");
+  });
+});
+
+//INTEGRATION TESTS
+//create a new group/“scope” called "<NumberOfEvents /> integration" via the describe() function
+describe("<NumberOfEvents /> integration", () => {
+  //create test described as "renders a new number of events based on what the user types as a number"
+  test("renders a new number of events based on what the user types as a number", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const numberOfEventsDOM = AppDOM.querySelector("#number-of-events");
+    const numberTextBox = within(numberOfEventsDOM).queryByRole("textbox");
+    await user.click(numberTextBox);
+    await userEvent.type(numberTextBox, "{backspace}{backspace}10");
+
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(10);
+    });
   });
 });
