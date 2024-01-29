b28e8cfa214aeccb6ddc1709fe194b313743a457
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-29 14:22:05 +0100

    Create showHideAnEventsDetails.test.js
    Create showHideAnEventsDetails.test.js file at features folder for acceptance testing. Import built-in functions from jest-cucumber. Use loadFeature() to load a "Gherkin" file called showHideAnEventsDetails.feature. Use defineFeature() to define the code for the loaded Gherkin file.
    Use "npm run test" command in the terminal and expect error message: "Test suite failed to run".

---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,8 @@
+//import built-in functions from jest-cucumber
+import { loadFeature, defineFeature } from "jest-cucumber";
+
+//use loadFeature() to load a Gherkin file (.feature)
+const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
+
+//use defineFeature() to define the code for the loaded Gherkin file (.feature)
+defineFeature(feature, (test) => {});
