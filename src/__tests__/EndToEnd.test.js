f0346047709272f387b412c110bce3014ec9004f
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-30 14:37:42 +0100

    Refactor tests at EndToEnd.test.js
    Use the beforeAll() and afterAll() to refactor end-to-end tests and with it reduce test run time.

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,51 +3,42 @@ import puppeteer from "puppeteer";
 
 //create a new scope using the describe() function
 describe("show/hide an event details", () => {
-  test("An event element is collapsed by default", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  let browser;
+  let page;
+  //use beforeAll()
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto("http://localhost:3000/");
 
     //use class selector for event element
     await page.waitForSelector(".event");
+  });
 
-    // use id selector for event's details
-    const eventDetails = await page.$(".event #details");
-    expect(eventDetails).toBeNull();
-
+  //use afterAll()
+  afterAll(() => {
     browser.close();
   });
 
-  test("User can expand an event to see its details", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
+  test("An event element is collapsed by default", async () => {
+    // use id selector for event's details
+    const eventDetails = await page.$(".event #details");
+    expect(eventDetails).toBeNull();
+  });
 
-    //use class selector for event element
-    await page.waitForSelector(".event");
+  test("User can expand an event to see its details", async () => {
     await page.click(".event .details-btn");
 
     // use id selector for event's details
     const eventDetails = await page.$(".event #details");
     expect(eventDetails).toBeDefined();
-
-    browser.close();
   });
 
   test("User can collapse an event to hide details", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
-
-    //use class selector for event element
-    await page.waitForSelector(".event");
-    await page.click(".event .details-btn");
     await page.click(".event .details-btn");
 
     // use id selector for event's details
     const eventDetails = await page.$(".event #details");
     expect(eventDetails).toBeNull();
-
-    browser.close();
   });
 });
