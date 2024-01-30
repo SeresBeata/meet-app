3ed39e4d62dcf4034791130b26d2fb197d88015f
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-30 14:29:12 +0100

    Create tests at EndToEnd.test.js
    Create end-to-end tests at EndToEnd.test.js file for "show/hide an event's details" feature.

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,4 +3,51 @@ import puppeteer from "puppeteer";
 
 //create a new scope using the describe() function
 describe("show/hide an event details", () => {
+  test("An event element is collapsed by default", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+
+    //use class selector for event element
+    await page.waitForSelector(".event");
+
+    // use id selector for event's details
+    const eventDetails = await page.$(".event #details");
+    expect(eventDetails).toBeNull();
+
+    browser.close();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+
+    //use class selector for event element
+    await page.waitForSelector(".event");
+    await page.click(".event .details-btn");
+
+    // use id selector for event's details
+    const eventDetails = await page.$(".event #details");
+    expect(eventDetails).toBeDefined();
+
+    browser.close();
+  });
+
+  test("User can collapse an event to hide details", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+
+    //use class selector for event element
+    await page.waitForSelector(".event");
+    await page.click(".event .details-btn");
+    await page.click(".event .details-btn");
+
+    // use id selector for event's details
+    const eventDetails = await page.$(".event #details");
+    expect(eventDetails).toBeNull();
+
+    browser.close();
+  });
 });
