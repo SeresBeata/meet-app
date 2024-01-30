35158d402df0840dfcac4b87272c497e362ea6f3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-01-30 18:10:50 +0100

    Create tests at EndToEnd.test.js
    Create a new scope using the describe() function and create end-to-end tests at EndToEnd.test.js file for "filter events by city" feature.

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -46,3 +46,55 @@ describe("show/hide an event details", () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+//create a new scope using the describe() function
+describe("filter events by city", () => {
+  let browser;
+  let page;
+  //use beforeAll()
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      //   headless: false,
+      //   slowMo: 250, // slow down by 250ms,
+      //   timeout: 0, // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+  });
+
+  //use afterAll()
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("User can see upcoming events from all cities", async () => {
+    await page.waitForSelector("#event-list");
+    const eventListItems = await page.$("#event-list .event");
+    expect(eventListItems).toBeDefined();
+  });
+
+  test("User can see a list of suggestions when searching for a city", async () => {
+    await page.waitForSelector("#city-search");
+    await page.click("#city-search .city");
+
+    await page.keyboard.down("Shift");
+    await page.keyboard.press("KeyA");
+    await page.keyboard.up("Shift");
+
+    await page.waitForSelector(".suggestions");
+    const citySuggestions = await page.$(".suggestions li");
+    expect(citySuggestions).toBeDefined();
+
+    await page.keyboard.press("Backspace");
+  });
+
+  test("User can expand an event to see its details", async () => {
+    await page.waitForSelector("#city-search");
+    await page.click("#city-search .city");
+    await page.type("#city-search .city", "Be");
+
+    await page.$eval(".suggestions li", (el) => (el.value = "Berlin, Germany"));
+
+    await page.click(".suggestions li");
+  });
+});
