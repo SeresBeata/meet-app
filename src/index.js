a8c3d92009c70be7a6c910b2ac5b71df1291689c
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-03 14:54:06 +0100

    Integrate Atatus at index.js file
    Import Atatus and use unique initialization/config code provided by the Atatus.
    Call the notify() function to check that Atatus has been integrated successfully.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
+//import Atatus
+import * as atatus from "atatus-spa";
+atatus.config("749dea90340b4148a9b849e7cd74aabd").install();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -21,3 +25,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//Use notify() to check if Atatus was successfully integrated
+atatus.notify(new Error("Test Atatus Setup"));
