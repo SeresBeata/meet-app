804cb5eb7d8850d7dbfb67dcdbc99c98f39b8075
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-18 15:53:55 +0100

    Create ErrorAlert subclass
    Create and export subclass named ErrorAlert at Alert.js file.

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -43,5 +43,14 @@ class InfoAlert extends Alert {
   }
 }
 
-//export InfoAlert
-export { InfoAlert };
+// create a subclass named ErrorAlert
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "rgb(255, 0, 0)"; // red
+    this.bgColor = "rgb(255, 220, 220)"; // light red
+  }
+}
+
+//export InfoAlert and ErrorAlert
+export { InfoAlert, ErrorAlert };
