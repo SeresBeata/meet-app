e7b3012dac8cab97afa38695af2a541322c17dcc
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-18 16:11:09 +0100

    Update NumberOfEvents.js with setErrorAlert
    Extract the setErrorAlert prop. Call setErrorAlert inside the handleInputChanged function. Use  isNaN() function.

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 //create and export NumberOfEvents child component
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   //create state variable, called "eventsNumber" with initial state 32.
   const [eventsNumber, setEventsNumber] = useState(32);
 
@@ -10,6 +10,18 @@ const NumberOfEvents = ({ setCurrentNOE }) => {
     const value = event.target.value;
     setEventsNumber(value);
     setCurrentNOE(value);
+
+    let errorText;
+    //use isNaN() function
+    if (isNaN(value) || value <= 0) {
+      errorText =
+        "You can only use positive numbers to set the number of events!";
+      setErrorAlert(errorText);
+    } else {
+      setCurrentNOE(value);
+      errorText = "";
+      setErrorAlert(errorText);
+    }
   };
 
   return (
