7eed35cc0e1008aeabc22f1027b08c471fc34f89
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-18 14:36:54 +0100

    Create Alert class component
    Create Alert.js file at components folder. Import  built-in Component from the React package. Create Alert class component.

---
+++ src/components/Alert.js
@@ -0,0 +1,35 @@
+//import the built-in React Component class from the React package
+import { Component } from "react";
+
+//create Alert class component
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+    this.bgColor = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      backgroundColor: this.bgColor,
+      borderWidth: "2px",
+      borderStyle: "solid",
+      fontWeight: "bolder",
+      borderRadius: "7px",
+      borderColor: this.color,
+      textAlign: "center",
+      fontSize: "12px",
+      margin: "10px 0",
+      padding: "10px",
+    };
+  };
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
