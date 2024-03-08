c1e919da8bb4679a1a56a9c8122a75d3871b355b
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-08 15:33:46 +0100

    Create CityEventsChart component
    Create CityEventsChart.js file and inside of it import useState, useEffect from react and import components from recharts. Create and export CityEventsChart child component.

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,20 @@
+//import from react
+import { useState, useEffect } from "react";
+
+//import components from recharts
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+//create and export CityEventsChart child component
+const CityEventsChart = () => {
+  return <div style={{ color: "#999" }}>This is a chart.</div>;
+};
+
+export default CityEventsChart;
