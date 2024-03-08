fdab89bdd106fd579966ac2ffba3b84a5ced8784
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-08 15:59:14 +0100

    Refactor CityEventsChart
    Create state variable, called "data" with initial state empty array. Create getData() function, and with it return the city name of a location and the event counts of a location. Use useEffect to call getData().

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -14,7 +14,30 @@ import {
 
 //create and export CityEventsChart child component
 const CityEventsChart = ({ allLocations, events }) => {
-  return <div style={{ color: "#999" }}>This is a chart.</div>;
+  //create state variable, called "data" with initial state empty array
+  const [data, setData] = useState([]);
+
+  //use useEffect with callback and call getData()
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  //create getData() function
+  const getData = () => {
+    //use map() on the allLocations array
+    const data = allLocations.map((location) => {
+      //filter the events by each location to get the length of the resulting array
+      //use that length value as the event count for the location
+      const count = events.filter(
+        (event) => event.location === location
+      ).length;
+      //use split() for the name of the city, without the country
+      const city = location.split(", ")[0];
+      //return the city name of a location and the event counts of a location
+      return { city, count };
+    });
+    return data;
+  };
 };
 
 export default CityEventsChart;
