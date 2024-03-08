9ab2e8bfa8a2d07007c4f747cbd588e72593256b
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-08 17:41:57 +0100

    Fix error caused by chart
    Resolve error (tests are failing with error message: "ReferenceError: ResizeObserver is not a constructor") by deleting window.ResizeObserver with beforeEach().

--- src/setupTests.js
+++ src/setupTests.js
@@ -22,3 +22,21 @@ console.error = (...args) => {
 
 // increase the timeout time for end-to-end tests, in which the headless mode is turned off
 jest.setTimeout(30000);
+
+// resolve error caused by adding chart with recharts
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
