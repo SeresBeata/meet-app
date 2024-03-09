666eb9dc5bdc4614a0ba564cc44b407abd751539
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-09 17:05:54 +0100

    Update EventGenresChart with Sample Chart
    Import components from recharts. Add Sample Pie Chart to EventGenresChart child component.

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,6 +1,29 @@
+//import components from recharts
+import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
+
 //create and export EventGenresChart child component
 const EventGenresChart = () => {
-  return <div style={{ color: "gray" }}>This is a chart.</div>;
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+  ];
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          labelLine={false}
+          label
+          outerRadius={130}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default EventGenresChart;
