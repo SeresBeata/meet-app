0da45071396ad00c1a057d1c581fd14517568d79
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-09 16:48:59 +0100

    Create EventGenresChart component
    Create EventGenresChart.js file and inside of it create and export EventGenresChart child component.

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,6 @@
+//create and export EventGenresChart child component
+const EventGenresChart = () => {
+  return <div style={{ color: "gray" }}>This is a chart.</div>;
+};
+
+export default EventGenresChart;
