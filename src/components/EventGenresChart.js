54b3f8fa2b3e08c7c58fde93c53bcea7e1261da2
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-09 17:40:34 +0100

    Refactor EventGenresChart
    Import useState and useEffect. Delete previously created const data. Create state variable, called "data" with initial state empty array. Create an array that includes "genres” (or event topics) that occur in the events. Create getData() function, and with it return how many events there are for each genre. Use useEffect to call getData().

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,15 +1,39 @@
+//import from react
+import { useState, useEffect } from "react";
+
 //import components from recharts
 import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
 
 //create and export EventGenresChart child component
-const EventGenresChart = () => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
 const EventGenresChart = ({ events }) => {
+  //create state variable, called "data" with initial state empty array
+  const [data, setData] = useState([]);
+  //create array for “genres” (or event topics) that occur in the events
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+
+  //use useEffect with callback and call getData()
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  //create getData() function, that returns how many events there are for each genre
+  const getData = () => {
+    //use .map()to loop over the genres array
+    const data = genres.map((genre) => {
+      //use filter() to get a list of events that include the current genre
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      //return an object that has two keys
+      //— a name key referring to the current genre in the .map() loop, and
+      //— a value that will refer to the filteredEvents array length.
+      return {
+        name: genre,
+        value: filteredEvents.length,
+      };
+    });
+    return data;
+  };
 
   return (
     <ResponsiveContainer width="99%" height={400}>
